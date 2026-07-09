b6e5d426c674d808972cad35b5b67a97d17b8052
anishmodidotcom <modianish11@gmail.com>
2026-07-09 21:40:07 +0530

    Delete sidonstage directory

--- sidonstage/lib/links.ts
+++
@@ -1,7 +0,0 @@
-export const LINKS = {
-  whatsapp: "https://wa.me/919811371501",
-  instagram: "https://www.instagram.com/sid.onstage",
-  youtube: "https://youtube.com/@siddhant.pamnani",
-};
-
-export const W3F_KEY = process.env.NEXT_PUBLIC_W3F_KEY ?? "";
